79ecc8021b3a8a0ff0899c96068bafa51303e59d
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-22 16:54:56 -0400

    Change all comments to responses

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :commenter, class_name: :User
-  belongs_to :questions, polymorphic: true
-  belongs_to :answers, polymorphic: true
-end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,5 @@
 class Response < ActiveRecord::Base
-  belongs_to :responder
+  belongs_to :responder, class_name: :User
+  belongs_to :questions, polymorphic: true
+  belongs_to :answers, polymorphic: true
 end
