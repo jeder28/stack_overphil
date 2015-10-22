d7ff5a838a449682f6ee570028ffed307ced99f5
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-22 16:32:16 -0400

    Change user_id to aliases

--- db/migrate/20151022195407_create_votes.rb
+++ db/migrate/20151022195407_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :value
-      t.integer :user_id
+      t.integer :voter_id
       t.integer :votable_id
       t.string :votable_type
 
