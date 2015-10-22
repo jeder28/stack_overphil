794d676bd4b984c7b00c7f3f4f5cad86049964c8
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-22 18:15:39 -0400

    Fix polymorphic associations

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,4 @@
 class Response < ActiveRecord::Base
   belongs_to :responder, class_name: :User
-  belongs_to :questions, polymorphic: true
-  belongs_to :answers, polymorphic: true
+  belongs_to :respondable, polymorphic: true
 end
