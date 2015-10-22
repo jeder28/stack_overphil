8b4676091656edb18463ac9c33b26eab9a671d22
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-22 17:37:54 -0400

    Change comment to responses

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveRecord::Base
     belongs_to :user, foreign_key: :asker_id
     has_many :answers
     has_many :tags, through: :taggings
-    has_many :comments, as: :commentable
+    has_many :responses, as: :respondable
     has_many :votes, as: :votable
 end
