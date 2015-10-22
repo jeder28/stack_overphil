3763db2981f78862e9e599683a4c3a52626fe4fd
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-22 17:00:40 -0400

    Fix dumb typos

--- db/migrate/20151022204735_create_responses.rb
+++ db/migrate/20151022204735_create_responses.rb
@@ -3,7 +3,7 @@ class CreateResponses < ActiveRecord::Migration
     create_table :responses do |t|
       t.string :content
       t.integer :responder_id
-      t.integer :respondableable_id
+      t.integer :respondable_id
       t.string :respondable_type
 
       t.timestamps null: false
