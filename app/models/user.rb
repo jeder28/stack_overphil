91872457ccc88065ab0efb00e7ceed17512fa085
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-22 17:59:00 -0400

    Add validations to user and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :answers, foreign_key: :answerer_id
   has_many :votes, foreign_key: :voter_id
   has_many :responses, foreign_key: :responder_id
+
+  has_secure_password
+  validates :username, :email, presence: true
 end
