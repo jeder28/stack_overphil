a3f67fc88e950cf6f50b0d3e95fe2f96defb8047
Phil Sofia <phil.sofia@gmail.com>
2015-10-23 13:01:47 -0400

    Add answer functionality to question show view

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,19 @@
+class AnswersController < ApplicationController
+  def create
+    @answer = Answer.new(params[:answer].permit(:content))
+    @answer.question = Question.find(params[:question_id])
+    #NEEDS TO BE CHANGED WHEN USER AUTHENTICATION IS SET
+    @answer.answerer = User.first 
+    if @answer.save
+      redirect_to question_path(@answer.question)
+    else
+      flash[:notice] = @answer.errors.full_messages.join(", ")
+      render :template => 'questions/show.html.erb', :question => Question.find(params[:question_id])
+    end
+  end
+
+  def new
+    
+  end
+
+end
