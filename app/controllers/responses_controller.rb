07cf8f72d332ba5dc4d1e21052e925bf2cf33dde
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-23 15:41:01 -0400

    Fill in create handling

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,14 +1,20 @@
 class ResponsesController < ApplicationController
 
-def new
-  # @params = params[:question_id]
-  # # @question = Question.find(params[:question])
-  @response = Response.new
-  @question = Question.find(params[:question_id])
+  def new
+    @response = Response.new
+    @question = Question.find(params[:question_id])
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @qustion.responses.build(responder_id: 1, content: params[:content])
+    if @question.save
+      redirect_to questions_show_path(@question)
+    else
+      flash[:message] = "Failed to comment successfully."
+      render :new
+    end
+  end
+
 end
 
-def create
-
-end
-
-end
