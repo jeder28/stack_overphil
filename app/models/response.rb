d0ebdf896a9b1355dbf81769efdfd5c1c3523c8d
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-23 16:21:33 -0400

    Add instance method to get the responder's username

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,9 @@ class Response < ActiveRecord::Base
   belongs_to :responder, class_name: :User
   belongs_to :respondable, polymorphic: true
   has_many :votes, as: :votable
+
+  def responder_name
+    user = User.find(self.responder_id)
+    user.username
+  end
 end
