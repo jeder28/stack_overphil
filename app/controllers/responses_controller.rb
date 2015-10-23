baf9a9f198e6404464eb0bbfd4c71df48c49ff52
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-23 13:14:18 -0400

    Create controller for responses

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,14 @@
+class ResponsesController < ApplicationController
+
+def new
+  # @params = params[:question_id]
+  # # @question = Question.find(params[:question])
+  @response = Response.new
+
+end
+
+def create
+
+end
+
+end
