63802481b340f3f3e8d22b8dd38c0c04394f66e9
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-23 13:37:27 -0400

    Added spec for question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { FactoryGirl.build :question }
+
+  it 'should array-ify a title for searching' do
+    question.title = 'Test title AeFt?'
+    question.save
+    expect(question.arrayify_title).to eq(['test', 'title', 'aeft'])
+  end
 end
