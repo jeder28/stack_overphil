e9ee70eaf5f5f7a7c58235d94202f345e0672ca8
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-10-22 20:58:44 -0400

    Created basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  resources :questions, only: [:index, :new, :create, :show] do
+    resources :answers, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:index, :new, :create, :show]
+      resources :votes, only: [:new, :create]
+    end
+    resources :comments, only: [:index, :new, :create, :show]
+    resources :votes, only: [:new, :create]
+
+    resources :users, only: [:new, :create, :show]
+    resources :sessions, only: [:new, :create, :delete]
+
+    get '/register' => 'user#new'
+    get '/login' => 'session#new'
+    get '/logout' => 'session#delete'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
