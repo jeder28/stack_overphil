07b46ed2abfdb3758721bbc07a605bed80c4d456
saraho-shea <sarahoshea56@gmail.com>
2015-10-24 18:18:47 -0400

    Add logic in tags model for creating tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,16 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :questions, through: :taggings
+
+  def self.make_tags(tags, question)
+    tag_array = tags.split(" ")
+    tag_array.each do |new_tag|
+        old_tag = Tag.find_by(name: new_tag)
+        if old_tag
+            question.tags.push(old_tag)
+        else
+            question.tags.create(name: new_tag)
+        end
+    end
+end
 end
