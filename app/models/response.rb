402bfc87a7129913b79054a1e87ce1722d0ac68f
Phil Sofia <phil.sofia@gmail.com>
2015-10-24 14:09:23 -0400

    Add count_votes method to response model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ActiveRecord::Base
+  before_save :count_votes
+  
   belongs_to :responder, class_name: :User
   belongs_to :respondable, polymorphic: true
   has_many :votes, as: :votable
@@ -6,5 +8,10 @@ class Response < ActiveRecord::Base
   def responder_name
     user = User.find(self.responder_id)
     user.username
+  end
+
+
+  def count_votes
+    self.vote_count = self.votes.sum(:value)
   end
 end
