58c9a660b24f34932290fa5471c4286287122bcd
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-23 22:25:33 -0400

    Fix responses controller for polymorphic responses on questions and answers

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,18 +1,35 @@
 class ResponsesController < ApplicationController
 
   def new
+    @parent = parent_object
+    @parent_name = @parent.class.name.downcase.pluralize
     @response = Response.new
-    @question = Question.find(params[:question_id])
   end
 
   def create
-    @question = Question.find(params[:question_id])
-    @question.responses.build(responder_id: current_user.id, content: params[:response][:content])
-    if @question.save
-      redirect_to question_path(@question)
+    @parent = parent_object
+    @response = @parent.responses.build(responder_id: current_user.id, content: params[:response][:content])
+    if @response.save
+      redirect_to parent_url(@parent)
     else
       flash[:message] = "Failed to comment successfully."
       render :new
+    end
+  end
+
+  private
+
+  def parent_object
+    case
+      when params[:question_id] then Question.find(params[:question_id])
+      when params[:answer_id] then Answer.find(params[:answer_id])
+    end
+  end
+
+  def parent_url(parent)
+    case
+      when params[:question_id] then question_path(parent)
+      when params[:answer_id] then question_path(parent.question_id)
     end
   end
 
