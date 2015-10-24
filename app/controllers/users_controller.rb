6ba481e26c555bc6c45226f20383440c927ca99b
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-23 23:41:32 -0400

    Fix register to save the user to session, add a show route for user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if (@user.save)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
