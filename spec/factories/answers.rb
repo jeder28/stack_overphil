54548e74afbd32d719d100d7d2810e9dcfef9004
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-24 16:22:17 -0400

    Fix answer factory to produce factorys of answers

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :answer do
     # responder_id 1
-    association :responder, factory: :user
+    association :answerer, factory: :user
     content Faker::Lorem.sentence
-    question
+    association :question, factory: :question
     # question_id Question.all.sample.id
     # association :question, factory: :question
   end
