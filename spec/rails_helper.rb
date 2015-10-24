860c66e49e22ec1cc0253b7878922502ff6c9f49
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-24 16:23:09 -0400

    Require database_cleaner in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'database_cleaner'
 
 require 'simplecov'
 # save to CircleCI's artifacts directory if we're on CircleCI
