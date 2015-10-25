d687137a08033febf0c7d92cce1a3ec3c890ad59
saraho-shea <sarahoshea56@gmail.com>
2015-10-25 19:51:09 -0400

    Add feature tests for login process

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Log in Process' do
+    let(:log_me_in) {
+        user = create(:user)
+        visit login_path
+        within("#login") do
+            fill_in 'Username', :with => user.username
+            fill_in 'Password', :with => user.password
+            click_button 'Login'
+        end
+    }
+    describe "If user is not logged in" do
+        it 'should have a sign up link' do
+          visit root_path
+          expect(page).to have_content('register')
+          expect(page).to have_content('login')
+      end
+  end
+end
