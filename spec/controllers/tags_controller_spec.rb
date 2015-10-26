c1682fe232bfb69008d09937b28ef744de967266
saraho-shea <sarahoshea56@gmail.com>
2015-10-25 22:33:08 -0400

    Add test for render index template

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -11,5 +11,10 @@ describe TagsController do
       get :index
       expect(response.status).to eq(200)
     end
+
+    it "renders index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 end
