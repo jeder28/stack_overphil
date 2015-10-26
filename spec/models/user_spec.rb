9742b5a7bbe359ae3b11b22de254de7e0fe54879
Heather Drain <hd.heatherdrain@gmail.com>
2015-10-26 00:14:39 -0400

    Initial shoulda matcher tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:questions).with_foreign_key('asker_id') }
+  it { should have_many(:answers).with_foreign_key('answerer_id') }
+  it { should have_many(:votes).with_foreign_key('voter_id') }
+  it { should have_many(:responses).with_foreign_key('responder_id') }
+
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:username) }
 end
