60443dbcd91668dd123a3ea9bdca413bf735ae36
saraho-shea <sarahoshea56@gmail.com>
2015-10-25 22:32:13 -0400

    Add test for status code

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 describe TagsController do
-  before(:each) do
+      before(:each) do
       @tag = create(:tag)
-      tag.questions << create(:question)
+      @tag.questions << create(:question)
+      end
+
+  describe "#index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
     end
   end
+end
